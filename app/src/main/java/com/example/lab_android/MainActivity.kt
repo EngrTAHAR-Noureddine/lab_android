b0a3379068f02002b8c0c146ee500605ac433e25
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-09-08 00:06:12 +0100

    "first try with appbar and grid view"

--- app/src/main/java/com/example/lab_android/MainActivity.kt
+++ app/src/main/java/com/example/lab_android/MainActivity.kt
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    //TODO read jetback and adapterview and recycle view gridview and extracts file xml
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+       // supportActionBar?.hide(); to hide app bar programaticlly
     }
+
+
+
+
 }
